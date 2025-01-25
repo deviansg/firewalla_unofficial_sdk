73c0f9c0c733ab9b33e721c9f51127eebf7a00fc
ian <ian@hurricanelabs.com>
2025-01-25 15:59:57 -0500

    Fixed broken tests

--- tests/test_firewalla.py
+++ tests/test_firewalla.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest.mock import patch
-from ..firewalla import Firewalla
+from firewalla import Firewalla
 
 @pytest.fixture
 def firewalla_instance():
@@ -43,12 +43,14 @@ def test_get_alarms(mock_get, firewalla_instance):
 
 @patch('firewalla.requests.get')
 def test_get_alarm(mock_get, firewalla_instance):
-    mock_response = {"id": 1, "alarm": "Alarm 1"}
+    mock_response = {
+        "results": [{"id": 1, "alarm": "Alarm 1"}]
+    }
     mock_get.return_value.json.return_value = mock_response
     mock_get.return_value.raise_for_status = lambda: None
 
     response = firewalla_instance.get_alarm(box_id="box1", alarm_id="alarm1")
-    assert response == mock_response
+    assert response == mock_response["results"]
 
 @patch('firewalla.requests.delete')
 def test_delete_alarm(mock_delete, firewalla_instance):
@@ -90,14 +92,25 @@ def test_get_flows(mock_get, firewalla_instance):
 
 @patch('firewalla.requests.get')
 def test_get_target_lists(mock_get, firewalla_instance):
-    mock_response = {
-        "results": [{"id": 1, "target": "Target 1"}, {"id": 2, "target": "Target 2"}]
-    }
+    mock_response = [
+        {
+            "id": "TL-00000000-0000-0000-0000-000000000000",
+            "name": "A Simple Target List",
+            "owner": "global",
+            "targets": [
+                "foo.com",
+                "bar.net"
+            ],
+            "category": "edu",
+            "notes": "This is a simple target list",
+            "lastUpdated": 1664373339.857
+        }
+    ]
     mock_get.return_value.json.return_value = mock_response
     mock_get.return_value.raise_for_status = lambda: None
 
     response = firewalla_instance.get_target_lists()
-    assert response == mock_response["results"]
+    assert response == mock_response
 
 @patch('firewalla.requests.get')
 def test_get_target_list(mock_get, firewalla_instance):
@@ -108,24 +121,3 @@ def test_get_target_list(mock_get, firewalla_instance):
     response = firewalla_instance.get_target_list(id=1)
     assert response == mock_response
     
-@patch('firewalla.requests.get')
-def test_get_boxes_no_group(mock_get, firewalla_instance):
-    mock_response = {
-        "results": [{"id": 1, "name": "Box 1"}, {"id": 2, "name": "Box 2"}]
-    }
-    mock_get.return_value.json.return_value = mock_response
-    mock_get.return_value.raise_for_status = lambda: None
-
-    response = firewalla_instance.get_boxes()
-    assert response == mock_response["results"]
-
-@patch('firewalla.requests.get')
-def test_get_boxes_with_group(mock_get, firewalla_instance):
-        mock_response = {
-            "results": [{"id": 1, "name": "Box 1"}]
-        }
-        mock_get.return_value.json.return_value = mock_response
-        mock_get.return_value.raise_for_status = lambda: None
-
-        response = firewalla_instance.get_boxes(group=1)
-        assert response == mock_response["results"]
