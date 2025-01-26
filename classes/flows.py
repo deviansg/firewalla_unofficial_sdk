ee087babe3d774ac263cb3daccaa577532e8dfe8
deviansg <iangillespie1982@gmail.com>
2025-01-26 15:00:54 -0500

    Functional --> Main (#1)
    * Made functional. Added tests.
    
    * Updated types; general cleanup
    
    * Fixed broken tests
    
    * Added github actions
    
    * Added requirements.txt
    
    * Added $GITHUB_ENV to $PATH
    
    * Fix broken curl url
    
    * Fix uv install to uv pip install
    
    * Add uv venv
    
    * Update from python 3.8 to 3.13
    
    * Test updates
    
    * uv updates - update github actions
    
    * update github actions
    
    * Update ci
    
    * Update ci
    
    * Added more documentation
    
    * Added codecov
    
    * Add codecov badge
    
    ---------
    
    Co-authored-by: ian <ian@hurricanelabs.com>

--- classes/flows.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict
-from classes.base import BaseFirewallaSDK
-
-class Flows(BaseFirewallaSDK):
-    
-    def get_flows(self, params: Dict = {"query": None, "groupBy": None, "sortBy": None, "limit": None, "cursor": None}):
-        return self._get("flows", params=params)
-            
--- classes/rules.py
+++
@@ -1,8 +0,0 @@
-from classes.base import BaseFirewallaSDK
-
-class Rules(BaseFirewallaSDK):    
-    def pause_rule(self, id: str) -> str:
-        return self._post(f"rules/{id}/pause")
-    
-    def resume_rule(self, id: str) -> str:
-        return self._post(f"rules/{id}/resume")
